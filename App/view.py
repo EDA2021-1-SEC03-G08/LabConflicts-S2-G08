b68f4855dbd2943a465741d412bfe59559754083
Hildovar <ha.pinzon@uniandes.edu.co>
2021-02-04 11:15:08 -0500

    Cambios del estudiante 2

--- App/view.py
+++ App/view.py
@@ -66,6 +66,7 @@ def loadBookTags():
     # TO-DO: Modificación de Est-1 en el Lab 2
 
 
+
 """
 Menu principal
 """
@@ -81,6 +82,7 @@ while True:
 
 
         # TO-DO: Modificación de Est-2 en el Lab 2
+        print("último libro: " + str(lt.lastElement(books)))
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
@@ -88,7 +90,10 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
     elif True:
         # TO-DO: Modificación de Est-2 en el Lab 2
-        pass
+    elif int(inputs[0]) ==3;
+    print("Cargando información de Book-Tags...")    
+    booktags = loadBookTags()
+    print("Total de Book-Tags cargados: " + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
